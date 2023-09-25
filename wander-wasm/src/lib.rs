6d6897efc4e5332f3af9732b4fc72bd327c8e9b0
Alex Michael Berry <alexmiberry@gmail.com>
2023-09-25 02:33:39 -0400

    adding wander-wasm, closes #2

---
+++ wander-wasm/src/lib.rs
@@ -0,0 +1,19 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+//! This project exposes functionality from the Rust implementation of Wander to WASM and JS runtimes thanks to wasm-bindgen and wasm-pack.
+
+mod utils;
+
+use wander::{WanderError, WanderValue};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn run(script: String) -> JsValue {
+    let mut bindings = wander::preludes::common();
+    match wander::run(&script, &mut bindings) {
+        Ok(value) => serde_wasm_bindgen::to_value(&value).unwrap(),
+        Err(err) => serde_wasm_bindgen::to_value(&Err::<WanderValue, WanderError>(err)).unwrap(),
+    }
+}
