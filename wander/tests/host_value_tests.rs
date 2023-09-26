08e546a8d5a82adf83e8f23320655bd249f80e86
Alex Michael Berry <alexmiberry@gmail.com>
2023-09-26 18:00:22 -0400

    adding tests for HostValues, closes #7

---
+++ wander/tests/host_value_tests.rs
@@ -0,0 +1,48 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use std::rc::Rc;
+
+use wander::interpreter::eval;
+use wander::parser::Element;
+use wander::preludes::common;
+use wander::{WanderValue, HostFunction, WanderType, WanderError, HostValue};
+
+struct SayHello {}
+impl HostFunction<String> for SayHello {
+    fn run(
+        &self,
+        _arguments: &[WanderValue<String>],
+        _bindings: &wander::bindings::Bindings<String>,
+    ) -> Result<WanderValue<String>, WanderError> {
+        Ok(WanderValue::HostValue(HostValue { value: "hello!".to_owned() }))
+    }
+
+    fn name(&self) -> String {
+        "hello".to_owned()
+    }
+
+    fn doc(&self) -> String {
+        "Say hello!".to_owned()
+    }
+
+    fn params(&self) -> Vec<WanderType> {
+        vec![]
+    }
+
+    fn returns(&self) -> WanderType {
+        WanderType::String
+    }
+}
+
+
+#[test]
+fn eval_boolean_true() {
+    let mut bindings = common::<String>();
+    bindings.bind_host_function(Rc::new(SayHello {}));
+    let input = vec![Element::FunctionCall("hello".to_owned(), vec![])];
+    let res = eval(&input, &mut bindings);
+    let expected = Ok(WanderValue::Boolean(true));
+    assert_eq!(res, expected);
+}
