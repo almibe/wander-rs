f3de9f2531c266d7715adcb826988140cd35c210
Alex Michael Berry <alexmiberry@gmail.com>
2023-10-07 16:54:27 -0400

    adding introspect fn to wander-wasm

--- wander-wasm/src/lib.rs
+++ wander-wasm/src/lib.rs
@@ -5,17 +5,16 @@
 //! This project exposes functionality from the Rust implementation of Wander to WASM and JS runtimes thanks to wasm-bindgen and wasm-pack.
 
 mod utils;
-
-use wander::{NoHostType, WanderError, WanderValue};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn run(script: String) -> JsValue {
-    let mut bindings = wander::preludes::common::<NoHostType>();
-    match wander::run(&script, &mut bindings) {
-        Ok(value) => serde_wasm_bindgen::to_value(&value).unwrap(),
-        Err(err) => {
-            serde_wasm_bindgen::to_value(&Err::<WanderValue<NoHostType>, WanderError>(err)).unwrap()
-        }
-    }
+    let mut bindings = wander::preludes::common::<wander::NoHostType>();
+    serde_wasm_bindgen::to_value(&wander::run(&script, &mut bindings)).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn introspect(script: String) -> JsValue {
+    let mut bindings = wander::preludes::common::<wander::NoHostType>();
+    serde_wasm_bindgen::to_value(&wander::introspect(&script, &mut bindings)).unwrap()
 }
